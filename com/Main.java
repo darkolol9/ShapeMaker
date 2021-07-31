6adf80811475446357fc20d5c426b5df05ba8952
darkolol9 <53996212+darkolol9@users.noreply.github.com>
2021-07-31 08:57:18 +0300

    ok

--- com/Main.java
+++ com/Main.java
@@ -3,13 +3,14 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        DrawingBoard board = new DrawingBoard();
+        Shape[] shapes = {new Ball(5),new Circle(4)};
+        DrawingBoard board = new DrawingBoard(shapes);
 
         board.add("circle",4);
         board.add("rectangle",5,6);
         // board.add("ball",8);
 
-        board.getMax().getInfo();
+        board.showAll();
 
     }
     
